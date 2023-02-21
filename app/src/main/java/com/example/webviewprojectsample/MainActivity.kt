7af57bb2bf56c5ede65fa41b03e2c9e3997a8114
codeandcodeandcode <codepkcode49@gmail.com>
2023-02-21 05:06:51 +0300

    I added the webViewClient and the url

--- app/src/main/java/com/example/webviewprojectsample/MainActivity.kt
+++ app/src/main/java/com/example/webviewprojectsample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.webviewprojectsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //WebViewClient allows you to handle
+        //onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        //this will load the url of the website
+        webView.loadUrl("https://sites.google.com/view/pkads")
+
+
     }
 }
